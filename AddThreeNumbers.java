ab071b9af030a958f0b4e2bd060252a452172697
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-01-14 23:50:03 +0530

    program to add 3 given number

---
+++ AddThreeNumbers.java
@@ -0,0 +1,11 @@
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        int n1,n2,n3,sum;
+        n1=10;
+        n2=20;
+        n3=30;
+        sum= n1 + n2 + n3 ;
+        System.out.println("the sum of three number is : ");
+        System.out.println(sum);
+    }
+}
