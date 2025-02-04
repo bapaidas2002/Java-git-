8c0b9c61a068c8d60ffed2d2ccdbb18b24ee4be1
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-02-04 23:18:20 +0530

    student information

--- Shapes.java
+++ Shapes.java
@@ -2,18 +2,18 @@ class circle{
     circle(int r){
         this.r=r;
     }
-    int area(int r){
+    int area(){
         double ar = 3.14*r*r;
         return ar;
     }
-    int circumference(int r){
-        double circum =2*3.14*r;
+    int circumference(){
+        double circum =2*3.14* r ;
         return circum;
     }
 }
 class Shapes{
     public static void main(String[] args) {
         circle c1 = new circle(10);
-        c1.circumference(0)
+        c1.circumference();
     }
 }
---
+++ StudentInformatio.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Student {
+    int eno;
+    String name;
+    float cgpa;
+    void Display(){
+        System.out.println("Student name is " + name);
+        System.out.println("Student enrollment number is "+eno);
+    }
+    void Percentage(){
+        float per;
+        per = (cgpa *100)/10;
+        System.out.println("Percentage of the student is "+per);
+    }   
+}
+class StudentInformatio {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Student s1=new Student();
+    Student s2=new Student();
+    System.out.print("Enter name of student :");
+    s1.name= sc.next();
+    System.out.print("Enter enrollnment of student :");
+    s1.eno= sc.nextInt();
+    System.out.print("Enter cgpa of student :");
+    s1.cgpa= sc.nextFloat();
+
+    s1.Display();
+    s2.Percentage();
+
+    }
+
+}
