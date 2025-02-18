1cac3f4b333ddcfcb01323d0c4fa472ddbe7319c
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-02-18 10:09:30 +0530

    changes

--- Main1.java
+++ Main1.java
@@ -1,28 +1,25 @@
 class A{
-    double x , y;
-    A(double x , double y){
-        this.x=x;
-        this.y=y;
-    }
+    double x,y;
     public double Sum2D(){
         return x+y;
     }
 }
 class B{
     double x , y, z;
-    B(double x , double y , double z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
-    }
+    
     public double Sum3D(){
         return x+y+z;
     }
 }
 class Main1{
     public static void main(String[] args) {
-        A a1=new A(2.0,3.1);
-        B b1=new B(3.2,4.2,5.3);
+        A a1=new A();
+        B b1=new B();
+        a1.x=2;
+        a1.y=3;
+        b1.x=3;
+        b1.y=4;
+        b1.z=5;
         System.out.println("the sum is : "+a1.Sum2D());
         System.out.println("the sum is : "+b1.Sum3D());
     }
