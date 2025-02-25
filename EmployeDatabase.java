44f166a33a3024f7e4f69ae8743badb06a1edb12
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-02-25 22:39:22 +0530

    changes

---
+++ EmployeDatabase.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+class EmployeDatabase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    System.out.println("ENter Details !");
+    System.out.print("Enter name : ");
+    String name =sc.nextLine();
+    System.out.print("Enter Day of Birth : ");
+    int day =sc.nextInt();
+    System.out.print("Enter Month of Birth : ");
+    int month =sc.nextInt();
+    System.out.print("Enter Year of Birth : ");
+    int year =sc.nextInt();
+    System.out.print("Enter Mobile Number : ");
+    Long mob =sc.nextLong();
+    System.out.print("Enter Person ID : ");
+    int id =sc.nextInt();
+    System.out.print("Enter Salary : ");
+    double sal =sc.nextDouble();
+    Employe e1 = new Employe();
+    Date dob =new Date(day,month,year);
+    e1.GetEmployeData(name,mob,dob, id, sal);
+    e1.DisplayEmploye();
+    }
+
+    
+}
+class Date{
+    int d,m,y;
+    Date(int d, int m,int y){
+        this.d=d;
+        this.m=m;
+        this.y=y;
+    }
+    public void DisplayDate(){
+        System.out.println("DOB : "+d+"/"+m+"/"+y);
+    }
+
+}
+class Person{
+    String name;
+    long number;
+    Date dob;
+    void GetPersonData(String n,long no,Date db){
+        name=n;
+        number=no;
+        dob=db;
+    }
+    void DisplayPerson(){
+        System.out.println("nme : "+name);
+        System.out.println("Mobile no : "+number);
+        dob.DisplayDate();
+    }  
+}
+class Employe extends Person{
+    int id ;
+    double salary;
+    void GetEmployeData(String n,long no,Date db,int id ,double salary){
+        this.id=id;
+        this.salary=salary;
+        GetPersonData(n,no,db);
+    }
+    void DisplayEmploye(){
+        DisplayPerson();
+        System.out.println("ID : "+id);
+        System.out.println("Salary : "+salary);    
+    }
+}
