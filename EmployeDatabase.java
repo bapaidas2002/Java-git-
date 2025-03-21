4c58127bf265888fe277201916aa596addac5a04
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-03-21 21:39:11 +0530

    changes

--- EmployeDatabase.java
+++ EmployeDatabase.java
@@ -64,7 +64,6 @@ class Employe extends Person{
     }
     void DisplayEmploye(){
         DisplayPerson();
-        DisplayPerson();
         
         System.out.println("Employe ID : "+id);
         System.out.println("Salary : "+salary);    
