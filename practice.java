bbc63489125f6c2c8b76438983193c2ce4fd071b
bapaidas2002 <146572716+bapaidas2002@users.noreply.github.com>
2025-04-05 23:45:15 +0530

    chnages

--- practice.java
+++ practice.java
@@ -1,10 +1,12 @@
 public class practice {
     public static void main(String[] args) {
         String name ="BapaiKumar";
+        String clas = "Btech cse second year";
         
         System.out.println(name.charAt(2));
         System.out.println(name.toLowerCase());
         System.out.println(name.toUpperCase());
+        System.out.println(clas.toUpperCase());
     }
     
 }
